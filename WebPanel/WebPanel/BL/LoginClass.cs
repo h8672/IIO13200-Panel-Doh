035e5f9d5a54972d7cf4ebf2c4db846920b1bc54
Juha-Matti Kokkonen <H8672@OHJE-322-WS-15>
2016-11-02 13:03:25 +0200

    Xml reading and Session attribute

--- WebPanel/WebPanel/BL/LoginClass.cs
+++ WebPanel/WebPanel/BL/LoginClass.cs
@@ -2,40 +2,83 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Xml;
+using System.IO;
 
 namespace WebPanel.BL
 {
     public class LoginClass
     {
-        private Boolean LoggedIn;
+        private Boolean logged;
         private String loginname;
+        private String path;
         //TODO huhu
         public String Loginname
         {
             get { return loginname; }
         }
 
+        public Boolean LoggedIn
+        {
+            get { return logged; }
+        }
+
+        LoginClass(String path)
+        {
+            this.path = path;
+            //TODO LoginClass
+            if (!File.Exists(this.path))
+            {
+                String[] contents = {
+                    "<users>",
+                        "<user>",
+                            "<username>Test</username>",
+                            "<password>me</password>",
+                            "<email>Email@Email.com</email>",
+                            "<securityQuestion>SecurityQuestion</securityQuestion>",
+                            "<securityAnswer>SecurityAnswer</securityAnswer>",
+                        "</user>",
+                    "</users>" };
+                File.WriteAllLines(this.path, contents);
+            }
+        }
+
         public String Login(String username, String password)
         {
             if (!LoggedIn)
             {
-                LoggedIn = TryLogin(username, password);
-                if (LoggedIn)
+                logged = TryLogin(username, password);
+                if (logged)
                 {
                     this.loginname = username;
                     return "Login successful!";
                 }
                 else return "Wrong username or password!";
             }
-            else return "You're already logged in!";
+            else return "You have already logged in!";
         }
 
         private Boolean TryLogin(String username, String password)
         {
             //TODO Check username and password from xml file!
-            
+            XmlDocument doc = new XmlDocument();
+            if (!File.Exists(this.path)) return false;
+            doc.Load(this.path);
+            XmlNodeList nodes = doc.SelectNodes("/users/");
+            Boolean found = false;
+            foreach (XmlNode item in nodes){
+                if (item["user"] != null)
+                {
+                    if(item.ChildNodes.Item(0).ToString() == username && item.ChildNodes.Item(1).ToString() == password)
+                    {
+                        //User exists with that username, password combination!
+                        found = true;
+                    }
+
+                }
+            }
             //If successful
-            return true;
+            return found;
         }
     }
 }
--- WebPanel/WebPanel/Masters/FirstMaster.Master.cs
+++ WebPanel/WebPanel/Masters/FirstMaster.Master.cs
@@ -11,13 +11,16 @@ namespace WebPanel.Masters
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //TODO Login check!
-            //if()
+            if (Session.IsNewSession) Session.Add("loginname", "");
+            //TODO If someone is logged in, forward page to user pages with user masterpage.
+            if (!(Session["loginname"] == "")) ;
+
+            //"~/Websites/FrontPage.aspx";
         }
 
         private Boolean CheckLogin(String username, String password)
         {
-            //Check login names and passwords from local xml file!
+            //TODO Check login names and passwords from local xml file!
 
             return true;
         }
@@ -28,49 +31,39 @@ namespace WebPanel.Masters
             e.Authenticated = CheckLogin(Login.UserName, Login.Password);
 
             //If login failed
-            if (!e.Authenticated)
-            {
-                main.Visible = true;
-            }
+            if (!e.Authenticated) return;
+            //Give session information that you have logged in
+            Session["loginname"] = Login.UserName;
             //Hide login button if login was succesful
-            else Button2.Visible = false;
-
-            //No need to hide login, it hides automatically when page reloads
+            Button2.Visible = false;
+            //TODO change destination elsewhere...
+            Login.DestinationPageUrl = "~/Websites/FrontPage.aspx";
         }
        
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            
+            //Loads Frontpage
         }
 
         protected void Button2_Click(object sender, EventArgs e)
         {
             //Show Login and hide contents
-            if (Register.Visible) Register.Visible = false;
-            else
-            {
-                main.Visible = false;
-                Login.Visible = true;
-            }
+            main.Visible = false;
+            Register.Visible = false;
+            Login.Visible = true;
         }
 
         protected void Button3_Click(object sender, EventArgs e)
         {
             //Show Register and hide contents
-            if (Login.Visible)
-            {
-                Login.Visible = false;
-            }
-            else
-            {
-                main.Visible = false;
-                Register.Visible = true;
-            }
+            main.Visible = false;
+            Login.Visible = false;
+            Register.Visible = true;
         }
         protected void Button4_Click(object sender, EventArgs e)
         {
-
+            //Loads Aboutpage
         }
     }
 }
